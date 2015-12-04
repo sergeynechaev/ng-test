d50ad73212afbca2577b03143e51d63f68bd92df
Sergey Nechaev <info@sergeynechaev.com>
2015-12-04 17:47:37 +0300

    initial

---
+++ src/app/about/about.controller.js
@@ -0,0 +1,12 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('ngTest')
+    .controller('AboutController', AboutController);
+
+  /** @ngInject */
+  function AboutController() {
+
+  }
+})();
---
+++ src/app/index.route.js
@@ -0,0 +1,25 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('ngTest')
+    .config(routeConfig);
+
+  function routeConfig($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainController',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'app/about/about.html',
+        controller: 'AboutController',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }
+
+})();
